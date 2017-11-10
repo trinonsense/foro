11832fac540ddcbac7344d68c6f1e77d791135de
Tri Noensie <tnnoensie@gmail.com>
2017-11-09 22:20:32 -0600

    eslint react rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
-  extends: 'standard',
+  extends: ['standard'],
+  plugins: ['react'],
   env: {
     browser: true,
     node: true
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   rules: {
   	'space-before-function-paren': ['error', 'never'],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
   	'operator-linebreak': 'off'
   }
 }
